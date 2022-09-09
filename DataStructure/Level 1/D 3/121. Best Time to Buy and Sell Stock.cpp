586e57e93d63e298896c6dea48ca20aaa373e4af
MinhazAbtahi <MINHAZ.FPGL@GMAIL.COM>
2022-09-09 08:30:23 +0600

    daily PS,DS,LC75

---
+++ DataStructure/Level 1/D 3/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        int size = prices.size();
+        
+        for (int i = 0; i < size; i++) {
+            minPrice = min(minPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+};
